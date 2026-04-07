67e3a54316c88a81b6e482f6e0390e10357ce8a7
wildanuwil <wildanfajriyann@gmail.com>
2026-04-07 09:03:59 +0700

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import {
+  Box,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
+import { createBrowserRouter, Link, RouterProvider } from 'react-router';
 
 const theme = createTheme({
   typography: {
@@ -6,10 +13,32 @@ const theme = createTheme({
   },
 });
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <Box>
+        <Typography variant="h2">Home</Typography>
+        <Link to="/login">Login</Link>
+      </Box>
+    ),
+  },
+  {
+    path: '/login',
+    element: (
+      <Box>
+        <Typography variant="h2">Login</Typography>
+        <Link to="/">Home</Link>
+      </Box>
+    ),
+  },
+]);
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
